b1bc28a01573788410dd5befc56b7ac43506e872
Reetaa99 <irisyazhi@gmail.com>
2022-02-05 23:39:17 -0800

    add windows html+css practice
    Initially I practiced on windows (before using github and mac), so upload my previous practice to github. Including my new project

---
+++ html+css/htmlpractice in bilibili/jsLearning/practice40p.js
@@ -0,0 +1,5 @@
+var myName = prompt('请输入您的名字');
+var myAge = prompt('请输入您的年龄');
+var mySex = prompt('请输入您的性别');
+var display = '您的名字是: ' + myName + '\n您的年龄是: ' + myAge + '\n您的性别是: ' + mySex;
+alert(display);
